5b72d650631d01e2b911b1f74d1c6a8024c8995d
Nicola Lerra <github.nicolalerra@gmail.com>
2022-09-26 17:11:02 +0200

    esercizio

---
+++ assets/Js/script.js
@@ -0,0 +1,24 @@
+// Richiamare la data attuale 
+
+let date = new Date()
+console.log(date)
+
+// Mostrarla in una pagina HTML 
+    
+document.getElementById('screen').innerHTML = date
+
+// Mostrare elementi singoli della data
+
+console.log(date.getDate())
+console.log(date.getHours())
+
+// Creare un sistema per la formattazione in formato europeo 
+
+let dateIT = date.toLocaleDateString('it-IT')
+
+
+// Mostrare la data formattata in una pagina HTML 
+
+document.getElementById('date').innerHTML = dateIT
+
+
